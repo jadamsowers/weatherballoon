b41f4a3e682c8403c75dfde9837b0e0c9b9f597d
J. Adam Sowers <adam.sowers@gmail.com>
2018-02-14 06:01:10 +0000

    actually transmitting APRS packets now

--- raspberrypi/python/aprs-payload.py
+++ raspberrypi/python/aprs-payload.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-import sys, time, logging, argparse, json, gpsd, aprs
+import os, sys, math, time, logging, argparse, json, gpsd, aprs
+from Adafruit_BME280 import *
 
+
+os.system('clear')
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', default="config.json", help="Path to the configuration file")
 parser.add_argument('-d', '--debug', help="Send debug information to console", action="store_true")
@@ -19,24 +22,55 @@ else:
 	logging.basicConfig(level=logging.INFO,format='%(levelname)s:%(message)s')
 
 config = json.load(open(args.config))
-print(config)
+logging.debug("Config file:\n" + json.dumps(config, indent=2, sort_keys=True))
 
-#frame = aprs.parse_frame(parser.callsign + ">APRS:>Weather balloon")
+callsign = config["aprs"]["callsign"]
+symbol = config["aprs"]["symbol"]
+table = config["aprs"]["table"]
+interface = config["aprs"]["interface"]
+hop = config["aprs"]["hop"]
+
+
+announce = callsign + ">APRS:>Weather balloon"
+logging.debug(announce)
+frame = aprs.Frame(announce)
 
 # connect to local gpsd instance
 logging.info('Connecting to gpsd')
 gpsd.connect()
 
-logging.info('Waiting for 3D GPS fix')
-time.sleep(5)
+logging.info('Connected to gpsd. Waiting for 3D GPS fix')
 packet = gpsd.get_current()
 while packet.mode < 3:
 	logging.info('still waiting...')
 	time.sleep(5)
+	packet = gpsd.get_current()
 
+logging.info('3D GPS fix found. Current position: ' + str(packet.position()))
+
+interval = int(config["aprs"]["interval"])
 while True:
-	
 	packet = gpsd.get_current()
 	logging.debug(packet)
-	
-	time.sleep(args.interval)
+
+	sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+	degrees = sensor.read_temperature()
+	pascals = sensor.read_pressure()
+	hectopascals = pascals / 100
+
+	sensorMessage = ' ' + '{0:.2f}'.format(degrees) + " deg C, " + '{0:.2f}'.format(hectopascals) + " hPa"
+	logging.info(sensorMessage)
+
+	hmsDate = str('%02d' % packet.get_time().hour + '%02d' % packet.get_time().minute + '%02d' % packet.get_time().second) + 'h'
+
+	lat = str(aprs.dec2dm_lat(packet.lat))
+	lon = str(aprs.dec2dm_lng(packet.lon))
+	track = str(packet.movement()['track']) + '/'
+	speed = str(packet.movement()['speed']) + '/'
+	alt = str(packet.alt)
+
+	aprsMessage = '\=' + hmsDate + lat + table + lon + symbol + track + speed + alt + sensorMessage
+	logging.info("Sending APRS message: " + aprsMessage)
+	#frame = aprs.Frame(aprsMessage)
+	os.system('beacon -d "' + hop + '" -s ' + interface + ' "' + aprsMessage + '"')
+	time.sleep(interval)
--- raspberrypi/scripts/prerequisites.sh
+++ raspberrypi/scripts/prerequisites.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-echo "Enabling SPI (requires reboot)"
-cat /boot/config.txt | sudo sed -e 's/\#\(dtparam=spi=on\)/\1/' > /boot/config.txt
+echo "Enabling SPI and UART (requires reboot)"
+sudo sed -i 's/\#\(dtparam=spi=on\)/\1/' /boot/config.txt
+echo "enable_uart=1" >> /boot/config.txt
 
 echo "Removing serial console (requires reboot)"
-cat /boot/cmdline.txt | sudo sed -e 's/console=serial0,115200 //' > /boot/cmdline.txt
+sudo sed -i 's/console=serial0,115200 //' /boot/cmdline.txt
 
 echo "Installing dependencies"
 sudo apt-get install python3 gpsd gpsd-clients ax25-tools ax25-apps
 
+echo "Installing Berryconda"
+wget https://github.com/jjhelmus/berryconda/releases/download/v2.0.0/Berryconda3-2.0.0-Linux-armv7l.sh
+chmod +x Berryconda3-2.0.0-Linux-armv7l.sh
+./Berryconda3-2.0.0-Linux-armv7l.sh -b
+
 echo "Installing python plugins"
-sudo pip install gpsd-py3 kiss==6.5.0 aprs==6.5.0
+sudo ~/berryconda3/bin/pip install gpsd-py3 aprs
+
+echo "Installing python libraries for sensor"
+git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+cd Adafruit_Python_GPIO
+sudo ~/berryconda3/bin/python setup.py install
+git clone https://github.com/adafruit/Adafruit_Python_BME280.git
+cd Adafruit_Python_BME280
+sudo ~/berryconda3/bin/python setup.py install
+
+echo "Installing pi-tnc apps"
+wget http://tnc-x.com/pitnc.zip
+unzip pitnc.zip
+chmod +x pitnc_*
+sudo mv pitnc_* /usr/local/sbin
+wget http://tnc-x.com/i2ckiss.zip
+unzip i2ckiss.zip
+chmod +x i2ckiss
+sudo mv i2ckiss /usr/local/sbin
+
+echo "Setting up gpsd"
+sudo service gpsd stop
+sudo sed -i 's/DEVICES=\"\"/DEVICES=\"\/dev\/ttyAMA0\"/' /etc/default/gpsd
+sudo service gpsd start
+
+
+#TODO: set up /etc/ax25/axports
